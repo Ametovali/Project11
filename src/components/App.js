5846bbb9b1b89d65aa6622ddca1285fefa89b8a7
Ali Ametov <ali_ametov@inbox.ru>
2022-05-05 17:10:05 +0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Header from './Header'
+import Order from './Order';
+import MenuAdmin from './MenuAdmin';
+import Burger from './Burger'
+import sampleBurgers from '../sample-burgers'
+
+class App extends React.Component{
+    state={
+        burgers:{},
+        order:{}
+    }
+
+    addBurger = burger=>{
+        //1.Делаем копию объекта state
+        const burgers = {...this.state.burgers};
+        //2.Добавить новый бургер в переменную burgers
+        burgers[`burger${Date.now()}`]=burger;
+        //3.Записать наш новый объект burgers в state
+        this.setState({burgers});
+    }
+
+    loadSampleBurgers = ()=>{
+        this.setState({burgers:sampleBurgers})
+    }
+
+
+    render(){
+        return(
+            <div className='burger-paradise'>
+                <div className='menu'>
+                    <Header title='Very Hot Burger' />
+                    <ul className='burgers'>
+                        {Object.keys(this.state.burgers).map(key => {
+                            return <Burger key = {key} 
+                            index={key}
+                            details={this.state.burgers[key]}
+                            
+                            />
+                        })}
+                    </ul>
+                </div>
+                <Order/>
+                <MenuAdmin addBurger={this.addBurger}
+                loadSampleBurgers={this.loadSampleBurgers}
+                />
+            </div>
+        )
+    }
+}
+
+
+export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,65 @@
+import React from "react";
+import restaurants from "../sample-restaurants";
+
+class Landing extends React.Component {
+  state = {
+    display: false,
+    title: "",
+    url: "",
+  };
+
+  displayList = () => {
+    const { display } = this.state;
+    this.setState({ display: !display });
+  };
+
+  getTitle=(restaurants)=>{
+      const {title, url} = restaurants;
+      this.setState({title, url,display:false});
+  };
+
+  goToRestaurant=()=>{
+    const {url} = this.state;
+    this.props.history.push(`/restaurant/${url}`)
+  };
+
+  render() {
+    return (
+      <div className="restaurant_select">
+        <div className="restaraunt_select_top">
+          <div
+            onClick={this.displayList}
+            className="restaraunt_select_top_header font-effect-outline"
+          >
+            {this.state.title? this.state.title :'ВЫБЕРИТЕ РЕСТОРАН'}
+          </div>
+          <div className="arrow_picker">
+            <div className="arrow_picker_up"></div>
+            <div className="arrow_picker_down"></div>
+          </div>
+        </div>
+        {this.state.display ? (
+          <div className="restaraunt_select_bottom">
+            <ul>
+              {restaurants.map((restaurants) => {
+                return (
+                  <li
+                    onClick={() => this.getTitle(restaurants)}
+                    key={restaurants.id}
+                  >
+                    {restaurants.title}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : null}
+        {this.state.title && !this.state.display?(
+           <button onClick={this.goToRestaurant}>Перейти в ресторан</button>
+        ):null}
+      </div>
+    );
+  }
+}
+
+export default Landing;
