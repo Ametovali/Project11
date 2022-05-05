cf518542a7bcca71823d956b5c9328e676e96c88
Ali Ametov <ali_ametov@inbox.ru>
2022-05-05 17:52:39 +0600

    Updates

---
+++ precache-manifest.9ef77bac2da6203321316788997622f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789fb6e81eaf687c040c7f8c6dd26658",
+    "url": "/Project11/index.html"
+  },
+  {
+    "revision": "0b30fc324cbd26f8df23",
+    "url": "/Project11/static/css/main.e016cb98.chunk.css"
+  },
+  {
+    "revision": "d2f57303c4c06f8588de",
+    "url": "/Project11/static/js/2.9e886f9b.chunk.js"
+  },
+  {
+    "revision": "0b30fc324cbd26f8df23",
+    "url": "/Project11/static/js/main.23fc44d1.chunk.js"
+  },
+  {
+    "revision": "767a4477495172af009a",
+    "url": "/Project11/static/js/runtime-main.b5080d3f.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/Project11/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/Project11/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/Project11/static/media/up.551b5c6b.svg"
+  }
+]);
